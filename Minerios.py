b951a81a9a1470221f49f85c661be6cbf3d695a8
Sinixtro <96551922+Sinixtro@users.noreply.github.com>
2022-01-05 18:27:10 -0300

    Update Minerios.py

--- Minerios.py
+++ Minerios.py
@@ -1,3 +1,4 @@
+
 print('-='*30)
 print('-='*30)
 print('(1)ferro')
@@ -13,32 +14,32 @@ print('-='*30)
 entrada_celsius = input('digite uma temperatura em celsius que deseje:')
 conv2 = float(entrada_celsius)
 
-if conv1 == 1:
-    pass
-elif conv2 <= 1543:
-    print(f'a temperatura {entrada_celsius}C não faz alterações na forma do ferro')
-elif conv2 >= 3000:
-    print(f'a temperatura {entrada_celsius}C faz o ferro entrar em ebolição')
-elif conv2 >= 1535:
-    print(f'a temperatura {entrada_celsius}C faz o ferro entrar em ponto de fusão')
-else:
-    ''''''
+def ferro_check (conv1):
+    if conv2  >= 1535:
+        print(f'A temperatura {entrada_celsius} faz o ferro entrar em ponto de fusão')
+    elif conv2 >= 3000:
+        print (f'A temperatura {entrada_celsius} faz o ferro entrar em ponto de ebulição')
+    else:
+        print(f'A temperatura {entrada_celsius} não faz alterações no ferro')
+
+def ouro_check (conv1):
+    if conv2 >= 1064:
+        print(f'A temperatura {entrada_celsius} faz o ouro entrar em ponto de fusão')
+    elif conv2 >= 2700:
+        print(f'A temperatura {entrada_celsius} faz o ouro entrar em ponto de fusão')
+    else:
+        print(f'A temperatura {entrada_celsius} não faz alterações no ouro')
+
+def check_minerios(conv1):
+    match conv1:
+     case 1:
+        ferro_check(conv2)
+     case 2:
+        ouro_check(conv2)
 
 
+check_minerios(conv1)
 
-if entrada_1 == 2:
-    pass
-elif conv2 <= 1063:
-    print(f'a temperatura {entrada_celsius}C não faz alterações na forma no ouro')
-
-elif conv2 >= 2700:
-    print(f'a temperatura {entrada_celsius}C faz o ouro entrar ebolição')
-
-elif conv2 > 1064:
-    print(f'a temperatura {entrada_celsius}C faz o ouro entrar em ponto de fusão')
-
-else:
-    ''''''
 
 
 
